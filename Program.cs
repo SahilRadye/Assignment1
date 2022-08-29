9ad2a3ca17e6ea0580d4eb9a241f050fa3c52ece
SahilRadye <111498539+SahilRadye@users.noreply.github.com>
2022-08-29 10:30:25 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalInfoLib;
+
+namespace TestPersonDetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****************************");
+            Person person = new Person("Sahil", "Radye", "sahilradye11@gmail.com", Convert.ToDateTime("10-11-2000"));
+            Console.WriteLine(person.ToString());
+
+            Console.WriteLine("*****************************");
+            Console.WriteLine(" Is the person is Adult :" + person.IsAdult());
+
+            Console.WriteLine("*****************************");
+            Console.WriteLine(" Today is a Birthday :" + person.Bday());
+
+            Console.WriteLine("*****************************");
+            Console.WriteLine(" Person Sun Sign :" + person.SunSign());
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
