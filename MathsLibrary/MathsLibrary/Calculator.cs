8e068f258ddbb9183910270361cbfa8d48ab4d0c
SahilRadye <111498539+SahilRadye@users.noreply.github.com>
2022-08-29 15:52:03 +0530

    Add files via upload

---
+++ MathsLibrary/MathsLibrary/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsLibrary
+{
+    public class Calculator
+    {
+
+        #region Fields And Properties
+        private int num1;
+
+        public int Num1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+
+        private int num2;
+        public int Num2
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+        #endregion
+
+        #region Methods
+        public int Addition()
+        {
+            return (num1 + num2);
+        }
+
+        public int Subtraction()
+        {
+            return (num1 - num2);
+        }
+        public int Multiply()
+        {
+            return (num1 * num2);
+        }
+        public int Division()
+        {
+            return (num1 / num2);
+        }
+        #endregion
+    }
+}
---
+++ MathsLibrary/MathsTestProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathsLibrary;
+
+namespace MathsTestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator calculator = new Calculator();
+            calculator.Num1 = 40;
+            calculator.Num2 = 20;
+
+
+            Console.WriteLine("Addition of Number = " + calculator.Addition());
+            Console.WriteLine("==============================================");
+
+            Console.WriteLine("Subtraction of Number = " + calculator.Subtraction());
+            Console.WriteLine("==============================================");
+
+            Console.WriteLine("Multiply of Number = " + calculator.Multiply());
+            Console.WriteLine("==============================================");
+
+            Console.WriteLine("Division of Number = " + calculator.Division());
+            Console.WriteLine("==============================================");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
