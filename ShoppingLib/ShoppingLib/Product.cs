fb37f84a0fbe34b3ef9e82425e5ac2b720c02615
SahilRadye <111498539+SahilRadye@users.noreply.github.com>
2022-08-29 15:49:50 +0530

    Add files via upload

---
+++ ShoppingLib/ShoppingLib/Product.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingLib
+{
+    public class Product
+    {
+
+        #region Field And Properties
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        private double price;
+
+        public double Price
+        {
+            get { return price; }   
+            set { price = value; }
+        }
+
+        private int quantity;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        #endregion
+
+        #region Constructor
+        public Product(string _productname, int _quantity, double _price)
+        {
+            ProductName = _productname;
+            Price = _price;
+            Quantity = _quantity;
+        }
+        #endregion
+
+        #region Method
+        public double GetProductCost()
+        {
+            return quantity * price;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"ProuctName = {ProductName},  Price = {Price} ,Quantity = {Quantity}, Cost = {Quantity*Price}");
+        }
+        #endregion
+
+
+    }
+}
---
+++ ShoppingLib/TestShopping/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingLib;
+
+namespace TestShopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("parleG" ,2 ,10);
+            Console.WriteLine(product1.ToString());
+
+            Console.WriteLine("*******************************************");
+            
+
+            Product product2 = new Product("bourbon", 4, 20);
+            Console.WriteLine(product2.ToString());
+
+            Console.WriteLine("*******************************************");
+
+            Product product3 = new Product("krackJack", 3, 15);
+            Console.WriteLine(product3.ToString());
+
+            Console.WriteLine("*******************************************");
+
+            Console.ReadLine();
+
+        }
+    }
+}
