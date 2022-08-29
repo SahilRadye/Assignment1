568bb22910eb683c99ad1d450abbaf5f35a39149
SahilRadye <111498539+SahilRadye@users.noreply.github.com>
2022-08-29 15:52:41 +0530

    Add files via upload

---
+++ OOPsDemoApp/OOPsDemoApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsDemoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine(rectangle); //To invoke ToString() Method
+            rectangle.Length = 30;
+            rectangle.Width = 10;
+
+            int area = rectangle.GetArea();
+
+            Console.WriteLine(rectangle.ToString());
+            Console.WriteLine("Area of rectangle = " + area);
+
+            
+
+            Rectangle rectangle1 = new Rectangle(5,2);
+
+            Console.WriteLine(rectangle1);
+            area = rectangle1.GetArea();
+            Console.WriteLine(area);
+
+
+            Console.WriteLine("=======================");
+            Console.WriteLine("Total count of Class : " + Rectangle.Counter);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OOPsDemoApp/OOPsDemoApp/Rectangle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//PascalCase : ProjName, ClassName, MethodName , PropertyName , EventName
+//camelCase :
+
+namespace OOPsDemoApp
+{
+    internal class Rectangle
+    {
+        //member field and method
+
+        //field
+
+        #region Fields And Properties
+        private int length; //camelCase
+
+        public int Length  // PascalCase
+        {
+            get { return length; }   //reading
+            set { length = value; }  //assignment
+        }
+
+        private int width; //camelCase
+
+        public int Width  // PascalCase
+        {
+            get { return width; }   //reading
+            set { width = value; }  //assignment
+        }
+        #endregion
+
+        #region Methods
+        public int GetArea()
+        {
+            return length * width;
+        }
+
+        public override string ToString()
+        {
+            //Traditional
+            //return "Length = " + Length + "Width = " + Width;
+
+            //Formatted 
+            //return String.Format("Length = {0} Width = {1}", Length , Width);
+
+            //String interpolation
+            return String.Format($"Length = {Length} Width = {Width}");
+        }
+
+        #endregion
+
+        #region Constructor
+        public Rectangle()
+        {
+            Length = 1;
+            Width = 1;
+            count++;
+        }
+
+        // parameterized constructor  
+
+        public Rectangle(int _length, int _width) :this()
+        {
+            Length = _length;
+            Width = _width;
+        }
+
+        #endregion
+
+        #region Static Members
+
+        private static int count;
+
+        public static int Counter
+        {
+            get { return count; }   
+        }
+
+        //static constructor
+        static Rectangle()
+        {
+            count = 100;
+        }
+        #endregion
+    }
+}
